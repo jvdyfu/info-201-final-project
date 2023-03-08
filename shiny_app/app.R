caaf3398d9128da35d8768943ccf3b51a261bec9
dkim6035 <dkim6035@uw.edu>
2023-03-07 23:34:29 -0800

    summary, source, visual

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -14,20 +14,34 @@ force <- read_delim("Use_Of_Force.csv")
 
 # I added my introduction slide to this document
 ui <- fluidPage(
-                titlePanel("Seattle Use Of Force Policing"),
+                titlePanel("Seattle Police Department: Use Of Force Policy"),
                 tabsetPanel(
-                  tabPanel("Introduction",
-                           mainPanel(
-                             h3("There are", nrow(force), "documented Use Of Force incidents in the City of Seattle"),
-                             HTML("<br>"),
+                  tabPanel("Introduction to Title 8",
+                           mainPanel(fluidRow(
+                             column(8,
+                            h2("Purpose:"),
+                             h5("Police brutality has risen around the country particularly with the targeting of marginalized communities (Black, Indigenous, Persons of Color, LGBTQIA+)
+                                which has began conversations surrounding (de)funding, (de)militarization, and the role of police in society as a whole. The Use of Force policy was implemented
+                                on 4/15/2021 in an attempt to ensure constant evaluation, impartial policing, and building accountability with the communities they serve. Wherever our peers
+                                stand on this debate, we believe this discourse and our opinions develop as new data uncovers patterns within policing."),
+                             
+                             h2("Source:"),
+                             h5("The data came the City of Seattle’s open data portal (data.seattle.gov) which contains data published by city departments. The department we chose to
+                                investigate was the Seattle Police Department, particularly SPD’s Use Of Force policy which is broken down in 4 levels:"),
+                             h5("Level 1 - Restraint/Transitory pain"),
+                             h5("Level 2 - Bodily harm"),
+                             h5("Level 3 - Bodily risk"),
+                             h5("Level 3 - Officer Involved Shooting (OIS)"),
+                             h3("Relevant information:"),
+                             h5("This dataset contained", nrow(force), "documented Use Of Force incidents in the City of Seattle."),
                              h5("The dataset reports incidents under eight different racial categories:", textOutput("race")),
-                             HTML("<br>"),
-                             h5("The dataset also breaks down the victims upon gender categories:", textOutput("gender")),
-                             HTML("<br>"),
-                             h5("The dataset operates under these Precincts/Areas:", textOutput("area")),
-                             img(src = "seattleprecinct.jpeg", height = 400, width = 300)
+                             h5("The dataset also breaks down the victims upon gender categories:", textOutput("gender"))),
+                             column(4,
+                                    HTML("<br>"),
+                                  h5("The dataset operates under these areas:", textOutput("area")),
+                             img(src = "seattleprecinct.jpeg", height = 400, width = 300))
                            )
-                  ),
+                  )),
                   tabPanel("Gender Bar Plot",
                            sidebarLayout(
                              sidebarPanel(
@@ -43,7 +57,8 @@ ui <- fluidPage(
                   ),
                   tabPanel("Table")
                 )
-)
+  )
+
 
 server <- function(input, output) {
   
